5805bfb19392c00689eb5e7a2eb9d64a7f73a942
LAPTOP-CNKFUT31\sanch <sanchezchris>
2020-10-23 21:01:52 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,122 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const exjwt = require('express-jwt');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+    next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const PORT = 3000;
+const secretKey = "secret key!";
+const jwtMW = exjwt({
+    secret: secretKey,
+    algorithms: ['HS256']
+});
+
+
+let users = [
+    {
+        id: 1,
+        username: 'chris',
+        password: 'sanchez'
+    },
+
+    {
+        id: 2,
+        username: 'pie',
+        password: 'test'
+    }
+];
+
+
+
+
+app.post('/api/login', (req, res) => {
+    const { username, password } = req.body;
+
+    for (user of users){
+        if (username == user.username && password == user.password){
+            let token = jwt.sign({ id:user.id, username: user.username}, secretKey, { expiresIn: '7d' });
+            res.json({
+                success: true,
+                err: null,
+                tokenValue: token
+            });
+            break;
+        }
+        else {
+            res.status(401).json({
+                success: false,
+                tokenValue: null,
+                err: "Username or Password is incorrect"
+            });
+        }
+    }
+
+});
+
+
+app.get('/api/dashboard', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'Secret Content for the initiated'
+    });
+});
+
+app.get('/api/settings', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'Secret Content for the initiated'
+    });
+    
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.use(function (err, req, res, next) {
+    if(err.name === 'UnauthorizedError') {
+        res.status(401).json({
+            success: false,
+            officialError: err,
+            err: "Username or Password is incorrect 2"
+        });
+    }
+    else{
+        next(err);
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Serving on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
